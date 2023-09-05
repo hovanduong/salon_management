8712ba1f3a981376d80b937665ad07b716e62277
LeeMinHooHue <130757891+LeeMinHooHue@users.noreply.github.com>
2023-09-05 21:41:39 +0700

    feat: update-cleancode

--- lib/src/presentation/category/category_screen.dart
+++ lib/src/presentation/category/category_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../configs/configs.dart';
+import '../../configs/constants/app_space.dart';
 import '../base/base.dart';
 import 'category_viewmodel.dart';
 
@@ -36,37 +37,41 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   Widget buildCategory() {
     return Scaffold(
-        appBar: AppBar(
-          title: Paragraph(
-            content: 'Category',
-            style: STYLE_LARGE_BOLD.copyWith(),
+      appBar: AppBar(
+        title: Paragraph(
+          content: 'Category',
+          style: STYLE_LARGE_BOLD.copyWith(),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return buildContentCategoryWidget(categories, index);
+        },
+      ),
+    );
+  }
+}
+
+Widget buildContentCategoryWidget(List<Category> categories, int index) {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: SizeToPadding.sizeVerySmall),
+    child: ExpansionTile(
+      title: Text(categories[index].name),
+      children: [
+        Padding(
+          padding: EdgeInsets.all(SizeToPadding.sizeSmall),
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: categories.length,
+            itemBuilder: (context, service5Index) {
+              return ListTile(
+                title: Text(categories[index].name),
+              );
+            },
           ),
         ),
-        body: ListView.builder(
-          itemCount: categories.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: ExpansionTile(
-                title: Text(categories[index].name),
-                children: [
-                  // Hiển thị nội dung danh mục ở đây
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: categories.length,
-                      itemBuilder: (context, serviceIndex) {
-                        return ListTile(
-                          title: Text(categories[index].name),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ));
-  }
+      ],
+    ),
+  );
 }
