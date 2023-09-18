205028e905d36819222b00c174636f986f5c5a9d
lethanhhoa892002 <130757802+lethanhhoa892002@users.noreply.github.com>
2023-09-18 12:00:18 +0700

    feat: Add logout

--- lib/src/presentation/profile/profile_viewmodel.dart
+++ lib/src/presentation/profile/profile_viewmodel.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/cupertino.dart';
 
+import '../../utils/app_pref.dart';
+import '../../utils/http_remote.dart';
 import '../base/base.dart';
 import '../routers.dart';
 
 class ProfileViewModel extends BaseViewModel {
   dynamic init() {}
 
-  Future<void> goToCategory(BuildContext context)
-    => Navigator.pushNamed(context, Routers.category);
+  Future<void> goToCategory(BuildContext context) =>
+      Navigator.pushNamed(context, Routers.category);
+
+  Future<void> goToSignIn(BuildContext context) =>
+      Navigator.pushReplacementNamed(context, Routers.signIn);
+
+  Future<void> logOut() async {
+    await AppPref.logout();
+    await HttpRemote.init();
+    await goToSignIn(context);
+  }
 }
