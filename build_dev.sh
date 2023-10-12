3f4eae518435c1d0fc8150ee48c2cd23935c3038
hovanduong <hovanduong8963@gmail.com>
2023-10-12 20:31:46 +0700

    Update build_dev.sh

--- build_dev.sh
+++ build_dev.sh
@@ -1,6 +1,7 @@
 flutter clean
 flutter pub get
 echo "START BUILD APK FILE ..."
-flutter build apk --dart-define=DART_DEFINES_BASE_URL=https://customer-api.easysalon.vn/api/v2/
+flutter build apk
+# flutter build apk --dart-define=DART_DEFINES_BASE_URL=https://customer-api.easysalon.vn/api/v2/
 # flutter build apk --split-per-abi
 # flutter build ipa --export-options-plist=ios/exportOptions.plist
