b3272caa50c7513a7f0ab41ce2780276b0ac16ed
hovanduong <hovanduong8963@gmail.com>
2024-01-06 18:23:10 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,9 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:spa_app_management/src/presentation/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-
+    //1
   });
 }
