74e2a420ef881857f73450a16a1615766255ba9b
hovanduong <hovanduong8963@gmail.com>
2024-01-09 13:21:24 +0700

    Update build_android_product.sh

--- build_android_product.sh
+++ build_android_product.sh
@@ -1,5 +1,5 @@
 flutter clean
 flutter pub get
 cd android
-flutter build appbundle --dart-define=DART_DEFINES_BASE_URL=https://spa-api.dhysolutions.net/api
-fastlane distribution -env beta
+flutter build appbundle --dart-define=DART_DEFINES_BASE_URL=https://prod.spa.dhysolutions.net/api
+fastlane deploy -env beta
