54c0853eefc936cdd6dedc8cc8a1d0c9cf73a41f
hovanduong <hovanduong8963@gmail.com>
2024-01-30 20:19:32 +0700

    feat: Release 1.0.8

--- build_android_product.sh
+++ build_android_product.sh
@@ -2,7 +2,7 @@
 flutter clean
 flutter pub get
 flutter build appbundle --dart-define=DART_DEFINES_BASE_URL=https://prod.spa.dhysolutions.net/api
-fastlane build_notify_ABB --env beta
-fastlane deploy --env beta
-fastlane build_notify_CHPlay --env beta
+# fastlane build_notify_ABB --env beta
+# fastlane deploy --env beta
+# fastlane build_notify_CHPlay --env beta
 
